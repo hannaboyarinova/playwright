36dd7b5473d567eaf77c19e46b3fba41f0424325
hannaboyaronova <hanna.boyarinova@gmail.com>
2026-06-03 05:20:25 -0400

    Initial commit

---
+++ tests/workshop_1/workshop1.spec.ts
@@ -0,0 +1,31 @@
+import {test} from '@playwright/test';
+
+test.skip('Basic navigation', async ({page}) => {
+    await page.goto('http://playwright.dev/docs/writing-tests');
+    await page.waitForTimeout(3000);
+    await page.reload();
+
+});
+
+test.skip('Interacting with elements', async ({page}) => {
+    await page.goto('https://about.gitlab.com/');
+    await page.waitForTimeout(3000);
+    await page.click("#onetrust-accept-btn-handler");
+    await page.getByRole('link', {name: 'Get free trial'}).click();
+    await page.waitForTimeout(5000);
+    await page.getByTestId('new-user-first-name-field').fill('Hanna');
+    await page.getByTestId('new-user-last-name-field').fill('Boyarinova');
+
+
+
+/* 
+    await page.getByRole("link", {name: 'actionability'}).click();
+    await page.waitForTimeout(5000); */
+});
+
+test('using Various Selectors', async ({page}) => {
+     await page.goto('https://about.gitlab.com/');
+     await page.click("#onetrust-accept-btn-handler"); //cookie banner
+     await page.getByRole('menuitem', { name: 'Platform' }).hover();
+     await page.locator('a.navigation-dropdown-cards__card[data-ga-name="platform"]').click();
+    await page.click(':has-text("View pricing")');});
